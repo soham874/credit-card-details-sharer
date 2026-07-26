c4a7e9f6d084b6088e56a7d37d0cd048aee11535
Soham Choudhury <73630164+soham874@users.noreply.github.com>
2026-07-26 10:30:41 +0530

    Initial setup

---
+++ ccshareapp/src/main/java/com/soham/ccshareapp/CcshareappApplication.java
@@ -0,0 +1,13 @@
+package com.soham.ccshareapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcshareappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CcshareappApplication.class, args);
+	}
+
+}
---
+++ ccshareapp/src/test/java/com/soham/ccshareapp/CcshareappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soham.ccshareapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CcshareappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
